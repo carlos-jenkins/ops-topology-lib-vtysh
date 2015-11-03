c73b2974faea316c01028ba4b4e0ec39b6176a5c
David Diaz <dadiaz@hpe.com>
2015-11-03 16:11:03 -0600

    new: dev: Add parse function for show_vlan

--- lib/topology_lib_vtysh/parser.py
+++ lib/topology_lib_vtysh/parser.py
@@ -28,7 +28,6 @@ import re
 
 def parse_show_interface(raw_result):
     """
-
     Parse the 'show interface' command raw output.
 
     :param str raw_result: vtysh raw result string.
@@ -42,18 +41,18 @@ def parse_show_interface(raw_result):
             'admin_state': 'down',
             'autonegotiation': True,
             'conection_type': 'Half-duplex',
-            'hadrware': 'Ethernet',
+            'hardware': 'Ethernet',
             'input_flow_control': False,
             'interface_state': 'down',
-            'mac_address': 'aa:55:aa:55:00:43',
+            'mac_address': '70:72:cf:d7:d3:dd',
             'mtu': 0,
             'output_flow_control': False,
-            'port': 1,
+            'port': 7,
             'rx_crc_fcs': 0,
             'rx_dropped': 0,
-            'rx_input_bytes': 0,
-            'rx_input_error': 0,
-            'rx_input_packets': 0,
+            'rx_bytes': 0,
+            'rx_error': 0,
+            'rx_packets': 0,
             'speed': 0,
             'speed_unit': 'Mb/s',
             'state_description': 'Administratively down',
@@ -61,8 +60,8 @@ def parse_show_interface(raw_result):
             'tx_bytes': 0,
             'tx_collisions': 0,
             'tx_dropped': 0,
-            'tx_input_errors': 0,
-            'tx_output_packets': 0
+            'tx_errors': 0,
+            'tx_packets': 0
         }
     """
 
@@ -92,10 +91,10 @@ def parse_show_interface(raw_result):
         r'(?P<tx_collisions>\d+) collision'
     )
 
-    raw_result = re.match(show_re, raw_result)
-    assert raw_result
+    re_result = re.match(show_re, raw_result)
+    assert re_result
 
-    result = raw_result.groupdict()
+    result = re_result.groupdict()
     for key, value in result.items():
         if value.isdigit():
             result[key] = int(value)
@@ -103,4 +102,53 @@ def parse_show_interface(raw_result):
             result[key] = True
         elif value == 'off':
             result[key] = False
+    return result
+
+
+def parse_show_vlan(raw_result):
+    """
+    Parse the 'show vlan' command raw output.
+
+    :param str raw_result: vtysh raw result string.
+    :rtype: dict
+    :return: The parsed result of the show vlan command in a
+    dictionary of the form:
+
+     ::
+
+        {
+            '1': { 'name': 'vlan1',
+                   'ports': [''],
+                   'reason': 'admin_down',
+                   'reserved': None,
+                   'status': 'down',
+                   'vlan_id': '1'
+             },
+             '2': {
+                    'name': 'vlan2',
+                    'ports': ['7', '3', '8', 'vlan2', '1'],
+                    'reason': 'ok',
+                    'reserved': None,
+                    'status': 'up',
+                    'vlan_id': '2'
+            }
+        }
+
+    FIXME: find a regular expresion for "reserved" field, so it doesn't
+    break ports
+    """
+
+    vlan_re = (
+        r'(?P<vlan_id>\d+)\s+(?P<name>\S+)\s+(?P<status>\S+)\s+'
+        r'(?P<reason>\S+)\s*(?P<reserved>FIXME)?\s*(?P<ports>.*)?'
+    )
+
+    result = {}
+    for line in raw_result.splitlines():
+        re_result = re.search(vlan_re, line)
+        if re_result:
+            partial = re_result.groupdict()
+            partial['ports'] = partial['ports'].split(', ')
+            result[partial['vlan_id']] = partial
+
     return result
